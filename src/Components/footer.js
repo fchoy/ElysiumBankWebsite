8fd4bc660bb0064409b88bc42220da0452f7ae1d
felix <felixchoy97@gmail.com>
2023-06-03 03:37:02 -0700

    added interactivity to newsletter form and social media icons

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -4,6 +4,11 @@ import '../Components/footer.css'
 
 import { Col, Container, Form, Row, Button } from 'react-bootstrap';
 
+const formSubmit = (e) => {
+  e.preventDefault();
+  alert("Thank you for joining our newsletter.");
+}
+
 const footer = () => {
   return (
     <Container fluid="true" className="d-flex flex-lg-row flex-column mt-3 mb-3 justify-content-center align-items-center h-auto">
@@ -11,16 +16,16 @@ const footer = () => {
       <Row className="row col-lg-4 col-md-auto col-12 d-flex flex-column h-75 mb-lg-0 mb-5 footer-row m-auto">
         <Col className="col-auto text-center mb-2">Connect With Us</Col>
         <Col className="col-6 w-auto h-auto text-center d-flex flex-column flex-md-row justify-content-center align-items-center mb-md-0 mb-3">
-          <i className="bi bi-facebook col-lg-2 col-12" style={{fontSize: "2rem"}}/>
-          <i className="bi bi-instagram col-lg-2 col-12" style={{fontSize: "2rem"}}/>
-          <i className="bi bi-twitter col-lg-2 col-12" style={{fontSize: "2rem"}}/>
+          <a href="https://facebook.com" rel="noreferrer" target='_blank' className='col-lg-2 col-12'><i className="bi bi-facebook" style={{fontSize: "2rem", cursor : "pointer", color: "blue"}}/></a>
+          <a href="https://instagram.com" rel="noreferrer" target='_blank' className='col-lg-2 col-12'><i className="bi bi-instagram col-lg-2 col-12" style={{fontSize: "2rem", cursor : "pointer", color: "hotpink"}}/></a>
+          <a href="https://twitter.com" rel="noreferrer" target='_blank' className='col-lg-2 col-12'><i className="bi bi-twitter col-lg-2 col-12" style={{fontSize: "2rem", cursor : "pointer", color: "light-blue"}}/></a>
         </Col>
       </Row>
 
       <Row className="row col-lg-4 col-12 d-flex flex-column m-auto mb-lg-0 mb-5 h-auto footer-row">
         <Col className="text-center mb-3">Sign-up For Our Newsletter</Col>
         <Col className="h-auto">
-          <Form className="w-75 col-md-12 m-auto d-flex flex-row h-auto mb-md-0 mb-5">
+          <Form className="w-75 col-md-12 m-auto d-flex flex-row h-auto mb-md-0 mb-5" onSubmit={formSubmit}>
             <Form.Control type="email" placeholder="example@gmail.com" classname="email-form" required/>
             <Button variant="primary" type="submit" className="w-auto" style={{marginLeft : "0.25rem"}}>Submit</Button>
           </Form>
